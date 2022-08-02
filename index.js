2de7620b159cd7f6e1833bf8099d736a7d0e6881
Ahmed Onour <ahmedonour.49@gmail.com>
2022-08-02 08:41:17 +0200

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+var app = express();
+
+
+// MIDDELWAER
+app.use(express.static("public"));
+app.set('views', 'views');
+app.set('view engine', 'pug');
+// MIDDELWAER
+
+
+// HOME ROUTE
+app.get('/', function(req, res){
+    res.render('index');
+}   // end of app.get
+);
+// SIGN UP ROUTE
+app.get('/register', function(req, res){
+    res.render('signup');
+    
+})
+// LOGIN ROUTE
+app.get('/login', function(req, res){
+    res.render('login');    
+});
+app.listen(port, function(){
+    console.log(`Server is running on ${port}`);
+}   // end of app.listen
+);
+
